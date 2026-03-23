773d3ec95d847437003915fd9c329e7a6119bdab
Fayyaz Ahmad <fayyazkhan0825@gmail.com>
2026-03-23 13:35:21 +0530

    Add ComplaintsScreen widget with basic layout

---
+++ Complaints_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ComplaintsScreen extends StatelessWidget {
+  const ComplaintsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Complaints Screen"),
+      ),
+    );
+  }
+}
