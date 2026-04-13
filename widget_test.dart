d65468f00f93fff8f1856a76a53ebfe3d3dd2b02
PrernaKandpal <prernakandpal0@gmail.com>
2026-04-13 17:17:06 +0530

    Update widget_test.dart

--- widget_test.dart
+++ widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hostel_outpass/main.dart';
 
-void main() {
+void main() { 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const HostelOutpassApp());
