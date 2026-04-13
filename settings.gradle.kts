53fcb14b70c2751d13eeff8f617086228694788f
PrernaKandpal <prernakandpal0@gmail.com>
2026-04-13 17:40:34 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
             file("local.properties").inputStream().use { properties.load(it) }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
             require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+            flutterSdkPath 
         }
  
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
