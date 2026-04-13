0346e0c3a3c8f69c45d5390b73361babd5f77e15
PrernaKandpal <prernakandpal0@gmail.com>
2026-04-13 17:32:33 +0530

    Update firestore_user_service.dart

--- firestore_user_service.dart
+++ firestore_user_service.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../models/app_user.dart';
 
-class FirestoreUserService {
+class FirestoreUserService { 
   final CollectionReference<Map<String, dynamic>> _usersRef =
       FirebaseFirestore.instance.collection('users');
 
