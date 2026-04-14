abb2e98a6903375072c48f76fb8c44d734a9673b
pakhi kumari <jhapakhi58@gmail.com>
2026-04-15 01:04:56 +0530

    Update Complaints_screen.dart

--- Complaints_screen.dart
+++ Complaints_screen.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 class ComplaintsScreen extends StatelessWidget {
   const ComplaintsScreen({super.key});
-
-  @override
+ @override
   Widget build(BuildContext context) { 
     return const Scaffold( 
       body: Center(
