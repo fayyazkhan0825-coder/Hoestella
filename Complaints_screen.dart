cd44da6b9124efdb0f790eb8691dbf035b9d2b39
PrernaKandpal <prernakandpal0@gmail.com>
2026-04-14 15:21:20 +0530

    Update Complaints_screen.dart

--- Complaints_screen.dart
+++ Complaints_screen.dart
@@ -5,7 +5,7 @@ class ComplaintsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return const Scaffold( 
       body: Center(
         child: Text("Complaints Screen"),
       ),
